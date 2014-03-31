49f1f1b8f7dd6cc52cd7a71d6e1195e05c73f84b
Alan David Garcia <alandavidgarcia@gmail.com>
2014-03-31 09:40:40 -0400

    Set ig client and secret vars in setup to be shared using sharify for intra app use and update the mounting of the hashtags app

--- config.js
+++ config.js
@@ -7,10 +7,12 @@
 
 module.exports = {
   NODE_ENV: 'development',
-  PORT: 4000,
-  API_URL: 'https://api.instagram.com/v1'
+  PORT: 5000,
+  API_URL: 'https://api.instagram.com/v1',
   API_PORT: process.env.IG_API_PORT || null,
-  API_BASE_PATH: process.env.IG_BASE_PATH || ''
+  API_BASE_PATH: process.env.IG_BASE_PATH || '',
+  IG_CLIENT_ID: process.env.IG_CLIENT_ID || "87f4400b663c4c568ac2bd9a36b87b67",
+  IG_CLIENT_SECRET: process.env.IG_CLIENT_SECRET || "ace1dd176b674b75879a14d4fd175962"
 }
 
 // Override any values with env variables
