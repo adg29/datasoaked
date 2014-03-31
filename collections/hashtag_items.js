39f3c0cdaf54fcf55fe21c40bdd0b9642d2b5907
Alan David Garcia <alandavidgarcia@gmail.com>
2014-03-31 00:46:49 -0400

    Define hashtag item model and collection

---
+++ collections/hashtag_items.js
@@ -0,0 +1,28 @@
+// 
+// Collection for hashtag items.
+// 
+// [Sharify](https://github.com/artsy/sharify) lets us require the API url
+// and Backbone.sync is replaced with a server-side HTTP module in /lib/setup
+// using [Backbone Super Sync](https://github.com/artsy/backbone-super-sync).
+// This combined with [browerify](https://github.com/substack/node-browserify) 
+// makes it simple to share this module in the browser and on the server.
+// 
+
+var Backbone = require('backbone')
+  , sd = require('sharify').data
+  , HashtagItem = require('../models/hashtag_item');
+
+module.exports = HashtagItems = Backbone.Collection.extend({
+  
+  model: HashtagItem,
+
+  url: function() {
+    // /v1/tags/snow/media/recent?access_token=ACCESS-TOKEN
+    return sd.API_URL + '/tags/' + this.hashtag + '/media/recent?client_id' + IG_CLIENT_ID;
+  },
+
+  initialize: function(models, options) {
+    this.hashtag = options.hashtag;
+  }
+
+});
---
+++ models/hashtag_item.js
@@ -0,0 +1,19 @@
+// 
+// Model for hashtag_item.
+// 
+// [Sharify](https://github.com/artsy/sharify) lets us require the API url
+// and Backbone.sync is replaced with a server-side HTTP module in /lib/setup
+// using [Backbone Super Sync](https://github.com/artsy/backbone-super-sync).
+// This combined with [browerify](https://github.com/substack/node-browserify) 
+// makes it simple to share this module in the browser and on the server.
+//
+
+var Backbone = require('backbone')
+  , sd = require('sharify').data;
+
+module.exports = HashtagItem = Backbone.Model.extend({
+
+  url: function() {
+    return sd.API_URL + '/media/' + this.get('uid') + '?client_id' + IG_CLIENT_ID;
+  }
+});
