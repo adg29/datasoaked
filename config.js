bc092c1aa69fa08ef06825326713a0a5243ff47f
Alan David Garcia <alandavidgarcia@gmail.com>
2014-03-31 00:40:26 -0400

    Add api config vars

--- config.js
+++ config.js
@@ -8,7 +8,9 @@
 module.exports = {
   NODE_ENV: 'development',
   PORT: 4000,
-  API_URL: 'https://api.twitter.com'
+  API_URL: 'https://api.instagram.com/v1'
+  API_PORT: process.env.IG_API_PORT || null,
+  API_BASE_PATH: process.env.IG_BASE_PATH || ''
 }
 
 // Override any values with env variables
