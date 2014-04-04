a2d09009617708f64b8a318d2d80d2f0e864b433
Alan David Garcia <alandavidgarcia@gmail.com>
2014-04-04 10:53:33 -0400

    Update tag to something brand specific

--- apps/hashtags/settings.js
+++ apps/hashtags/settings.js
@@ -5,6 +5,6 @@ sd['debug'] = true;
 sd['httpClient'] = require('http');
 sd['REDIS_PORT'] = 6486;
 sd['REDIS_HOST'] = '127.0.0.1';
-sd['hashtag'] = 'tbt';
+sd['hashtag'] = 'liveaudio';
 sd['hashtag_items'] = 24;
 
