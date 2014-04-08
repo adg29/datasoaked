c068cced5e928cfba5c706694e3f5ec12213f07e
Alan David Garcia <alandavidgarcia@gmail.com>
2014-04-07 22:13:02 -0400

    Display twitter media in layout

--- apps/hashtags/templates/helpers.js
+++ apps/hashtags/templates/helpers.js
@@ -14,7 +14,7 @@ moment.fn.fromNoww = function (a) {
 }
 
 function debug(msg) {
-  if (sd.debug) {
+  if (false && sd.debug) {
     console.log(msg);
     if (msg instanceof Error)
       console.log(msg.stack)
