b7fa05fdc7731c6dac5e66824477f734f6c42df0
Alan David Garcia <alandavidgarcia@gmail.com>
2015-02-13 20:37:37 -0500

    Assets and IG keys

--- apps/hashtags/settings.js
+++ apps/hashtags/settings.js
@@ -10,11 +10,18 @@ sd['REDIS_HOST'] = '127.0.0.1';
  sd['IG_API_URL'] = 'https://api.instagram.com/v1';
  sd['IG_API_PORT'] = process.env.IG_API_PORT || null;
  sd['IG_API_BASE_PATH'] = process.env.IG_BASE_PATH || '';
- sd['IG_CLIENT_ID'] = process.env.IG_CLIENT_ID || "fbc600c2d36d4c5dae64ae9a275ba612";
- sd['IG_CLIENT_SECRET'] = process.env.IG_CLIENT_SECRET || "a8ab4b3ef5bc4486b04a4fa97b9ac6cb";
+
+ // sd['IG_CLIENT_ID'] = process.env.IG_CLIENT_ID || "fbc600c2d36d4c5dae64ae9a275ba612";
+ // sd['IG_CLIENT_SECRET'] = process.env.IG_CLIENT_SECRET || "a8ab4b3ef5bc4486b04a4fa97b9ac6cb";
+
+
+
  //sd['IG_CLIENT_ID'] = process.env.IG_CLIENT_ID || "602782ce658f4577b7950bea45e510cd";
  //sd['IG_CLIENT_SECRET'] = process.env.IG_CLIENT_SECRET || "8f64948615734619a8d847646152048d";
 
-sd['hashtag'] = 'liveaudio';
+sd['IG_CLIENT_ID'] = "7215ab31c2b5410ab649f11d6ff060df"
+sd['IG_CLIENT_SECRET'] ="5b3bcecdfe24425384559b961a91f084"
+
+sd['hashtag'] = 'tulum';
 sd['hashtag_items'] = 60;
 
